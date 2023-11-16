89526201e3425f3c2e76c10578803338a602325b
igor21211 <spuraigor@gmail.com>
2023-11-17 00:58:11 +0300

    working

---
+++ src/js/model.js
@@ -0,0 +1,47 @@
+import { API_URL } from './config.js';
+import { getJson } from './helpers.js';
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJson(`${API_URL}${id}`);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.sourceUrl,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJson(`${API_URL}?search=${query}`);
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
